2ea3ab84bcab213f54288433f2296220d35b32b1
Komie <joshua.komie603@bl1330-cs-03.local>
2015-08-31 16:14:20 -0700

    added header files

---
+++ udpsocklib.h
@@ -0,0 +1,24 @@
+/* udpsocklib is a light framework to send and recieve udp packets with simple function calls.
+ *
+ *
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MAXBUFLEN 100
+#define DEBUG_MODE 1
+/*
+ *
+ */
+int sendPacket(char* ipAddress, char* port, char* data);
+
+int listenForPacket(char* port, char*packetData, int *packetSize);
